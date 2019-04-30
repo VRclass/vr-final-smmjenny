e225148f00bb551caf18c04d77b36b650f336bd1
Meimei <meimeisong2018@gmail.com>
2019-04-29 22:14:30 -0500

    sound script

---
+++ Final_Project_smmjenny_2019/Assets/Script/MyPlaySound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MyPlaySound : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource source;
+    public bool alreadyPlayed = false;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            source.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+    }
+
+}
